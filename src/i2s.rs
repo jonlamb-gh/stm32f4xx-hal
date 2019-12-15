13dfad236bb3d2789cbb9b2a3268ca62ddb5ed13
Jon Lamb <lamb.jon.io@gmail.com>
2019-12-15 06:45:26 -0800

    Use PINS pattern

--- src/i2s.rs
+++ src/i2s.rs
@@ -3,7 +3,6 @@
 // NOTE: based on https://github.com/astro/stm32f429-hal/blob/master/src/i2s.rs
 
 // TODO
-// - port to the PINS pattern used in this crate, like in SPI
 // - macro gen for I2S 2 and 3
 // - as master mode support
 
@@ -14,35 +13,45 @@ use crate::rcc::Clocks;
 use crate::stm32::{RCC, SPI2};
 use core::marker::PhantomData;
 
+pub trait Pins<I2S> {}
+
 /// SD: Serial Data (mapped on the MOSI pin) to transmit or receive
 /// the two time- multiplexed data channels (in half-duplex mode
 /// only).
-pub unsafe trait SdPin<I2S> {}
-unsafe impl SdPin<SPI2> for PB15<Alternate<AF5>> {}
+pub trait PinSd<I2S> {}
+impl PinSd<SPI2> for PB15<Alternate<AF5>> {}
 
 /// WS: Word Select (mapped on the NSS pin) is the data control signal output in master
 /// mode and input in slave mode.
-pub unsafe trait WsPin<SPI1> {}
-unsafe impl WsPin<SPI2> for PB9<Alternate<AF5>> {}
-unsafe impl WsPin<SPI2> for PB12<Alternate<AF5>> {}
+pub trait PinWs<SPI1> {}
+impl PinWs<SPI2> for PB9<Alternate<AF5>> {}
+impl PinWs<SPI2> for PB12<Alternate<AF5>> {}
 
 /// CK: Serial Clock (mapped on the SCK pin) is the serial clock output in master mode
 /// and serial clock input in slave mode.
-pub unsafe trait CkPin<I2S> {}
-unsafe impl CkPin<SPI2> for PB10<Alternate<AF5>> {}
-unsafe impl CkPin<SPI2> for PB13<Alternate<AF5>> {}
+pub trait PinCk<I2S> {}
+impl PinCk<SPI2> for PB10<Alternate<AF5>> {}
+impl PinCk<SPI2> for PB13<Alternate<AF5>> {}
 
 /// SPI2ext_SD and SPI3ext_SD: additional pins (mapped on the MISO pin) to control the
 /// I 2 S full duplex mode.
-pub unsafe trait ExtSdPin<I2S> {}
-unsafe impl ExtSdPin<SPI2> for PB14<Alternate<AF6>> {}
+pub trait PinExtSd<I2S> {}
+impl PinExtSd<SPI2> for PB14<Alternate<AF6>> {}
 
 /// MCK: Master Clock (mapped separately) is used, when the I 2 S is configured in master
 /// mode (and when the MCKOE bit in the SPI_I2SPR register is set), to output this
 /// additional clock generated at a preconfigured frequency rate equal to 256 × F S , where
 /// F S is the audio sampling frequency.
-pub unsafe trait MckPin<I2S> {}
-unsafe impl MckPin<SPI2> for PC6<Alternate<AF5>> {}
+pub trait PinMck<I2S> {}
+impl PinMck<SPI2> for PC6<Alternate<AF5>> {}
+
+impl<I2S, SD, CK, WS> Pins<I2S> for (SD, CK, WS)
+where
+    SD: PinSd<I2S>,
+    CK: PinCk<I2S>,
+    WS: PinWs<I2S>,
+{
+}
 
 /// Slave role (doesn't provide clock)
 pub struct SlaveRole {}
@@ -63,22 +72,18 @@ pub enum I2sStandard {
 
 /// I2S peripheral
 #[allow(unused)]
-pub struct I2s<SPI, SD, CK, WS> {
+pub struct I2s<SPI, PINS> {
     spi: SPI,
-    sd: SD,
-    ck: CK,
-    ws: WS,
+    pins: PINS,
 }
 
 /// I2S peripheral
 #[allow(unused)]
-pub struct I2sOutput<Role, Data, SPI, SD, CK, WS> {
+pub struct I2sOutput<Role, Data, SPI, PINS> {
     role: PhantomData<Role>,
     data: PhantomData<Data>,
     spi: SPI,
-    sd: SD,
-    ck: CK,
-    ws: WS,
+    pins: PINS,
 }
 
 /// Implemented by data types that fit the device's data width: `u16`,
@@ -112,12 +117,10 @@ impl I2sData for u32 {
 }
 
 /// I2S interface on SPI pins
-impl<SD, CK, WS> I2s<SPI2, SD, CK, WS> {
-    pub fn i2s2(spi: SPI2, sd: SD, ck: CK, ws: WS, _clocks: Clocks) -> Self
+impl<PINS> I2s<SPI2, PINS> {
+    pub fn i2s2(spi: SPI2, pins: PINS, _clocks: Clocks) -> Self
     where
-        SD: SdPin<SPI2>,
-        CK: CkPin<SPI2>,
-        WS: WsPin<SPI2>,
+        PINS: Pins<SPI2>,
     {
         // NOTE(unsafe) This executes only during initialisation
         let rcc = unsafe { &(*RCC::ptr()) };
@@ -128,14 +131,14 @@ impl<SD, CK, WS> I2s<SPI2, SD, CK, WS> {
         rcc.apb1rstr.modify(|_, w| w.spi2rst().set_bit());
         rcc.apb1rstr.modify(|_, w| w.spi2rst().clear_bit());
 
-        I2s { spi, sd, ck, ws }
+        I2s { spi, pins }
     }
 
     /// Configure in slave mode as output
     pub fn into_slave_output<S: I2sData>(
         self,
         standard: I2sStandard,
-    ) -> I2sOutput<SlaveRole, S, SPI2, SD, CK, WS> {
+    ) -> I2sOutput<SlaveRole, S, SPI2, PINS> {
         self.spi.i2scfgr.modify(|_, w| {
             unsafe {
                 // Select I2S mode
@@ -166,16 +169,14 @@ impl<SD, CK, WS> I2s<SPI2, SD, CK, WS> {
             role: PhantomData,
             data: PhantomData,
             spi: self.spi,
-            sd: self.sd,
-            ck: self.ck,
-            ws: self.ws,
+            pins: self.pins,
         }
     }
 }
 
-impl<'s, Role, S: I2sData + Sized + 's, SD, CK, WS> I2sOutput<Role, S, SPI2, SD, CK, WS> {
+impl<'s, Role, S: I2sData + Sized + 's, PINS> I2sOutput<Role, S, SPI2, PINS> {
     /// Disable and return `I2s`
-    pub fn into_i2s(self) -> I2s<SPI2, SD, CK, WS> {
+    pub fn into_i2s(self) -> I2s<SPI2, PINS> {
         // Wait
         while self.spi.sr.read().bsy().bit() || !self.spi.sr.read().txe().bit() {}
         // Disable first
@@ -183,9 +184,7 @@ impl<'s, Role, S: I2sData + Sized + 's, SD, CK, WS> I2sOutput<Role, S, SPI2, SD,
 
         I2s {
             spi: self.spi,
-            sd: self.sd,
-            ck: self.ck,
-            ws: self.ws,
+            pins: self.pins,
         }
     }
 
